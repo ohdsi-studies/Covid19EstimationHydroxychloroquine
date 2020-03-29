4f73a6f95fa27adea563023c8fab179e7df6c20a
Jamie Weaver <jweave17@its.jnj.com>
2020-03-28 22:20:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-[Study title]
+OHDSI COVID-19 Studyathon: Hydroxychloroquine population-level effect estimation
 =============
 
-<img src="https://img.shields.io/badge/Study%20Status-Repo%20Created-lightgray.svg" alt="Study Status: Repo Created">
+<img src="https://img.shields.io/badge/Study%20Status-Started-blue.svg" alt="Study Status: Started">
 
-- Analytics use case(s): **-**
-- Study type: **-**
-- Tags: **-**
-- Study lead: **-**
-- Study lead forums tag: **[[Lead tag]](https://forums.ohdsi.org/u/[Lead tag])**
-- Study start date: **-**
+- Analytics use case(s): **Population-Level Estimation**
+- Study type: **Clinical Application**
+- Tags: **Study-a-thon, COVID-19**
+- Study lead: **Jennifer Lane**
+- Study lead forums tag: **[jenniferlane](https://forums.ohdsi.org/u/jenniferlane)**
+- Study start date: **March 26, 2020**
 - Study end date: **-**
 - Protocol: **-**
 - Publications: **-**
-- Results explorer: **-**
+- Results explorer: **https://data.ohdsi.org/Covid19EstimationHydroxychloroquine/**
 
-[Description (single paragraph)]
+Many existing drugs are being considered for use in treatment and prophylaxis of COVID-19 in rapid clinical trials across the world. However, the full safety profiles of these drugs is often unknown, and the current trials are unlikely to be powered or have sufficent follow-up time to evaluate most safety outcomes. The aim of this OHDSI study is to use existing retrospective data to evaluate the safety of these drugs. Where possible, we also attempt to estimate potential efficacy, for example using prior viral infections as surrogate outcome, or where available by using COVID-19 as outcome.
 
-[You can add other text at this point]
+This study is part of the OHDSI 2020 COVID-19 study-a-thon.
+
+License
+=======
+The Covid19DrugRepurposing package is licensed under Apache License 2.0
---
+++ extras/CodeToRun.R
@@ -0,0 +1,56 @@
+library(Covid19EstimationHydroxychloroquine)
+
+# Optional: specify where the temporary files (used by the ff package) will be created:
+options(fftempdir = "")
+
+# Maximum number of cores to be used:
+maxCores <- parallel::detectCores()
+
+# The folder where the study intermediate and result files will be written:
+outputFolder <- ""
+
+# Details for connecting to the server:
+connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "pdw",
+                                                                server = Sys.getenv("server"),
+                                                                user = NULL,
+                                                                password = NULL,
+                                                                port = Sys.getenv("port"))
+
+# The name of the database schema where the CDM data can be found:
+cdmDatabaseSchema <- ""
+
+# The name of the database schema and table where the study-specific cohorts will be instantiated:
+cohortDatabaseSchema <- ""
+cohortTable <- ""
+
+# Some meta-information that will be used by the export function:
+# Please use a short and descriptive databaseId and databaseName, e.g. OptumDOD
+databaseId <- ""
+databaseName <- ""
+databaseDescription <- ""
+
+# For Oracle: define a schema that can be used to emulate temp tables:
+oracleTempSchema <- NULL
+
+execute(connectionDetails = connectionDetails,
+        cdmDatabaseSchema = cdmDatabaseSchema,
+        cohortDatabaseSchema = cohortDatabaseSchema,
+        cohortTable = cohortTable,
+        oracleTempSchema = oracleTempSchema,
+        outputFolder = outputFolder,
+        databaseId = databaseId,
+        databaseName = databaseName,
+        databaseDescription = databaseDescription,
+        createCohorts = TRUE,
+        synthesizePositiveControls = FALSE,
+        runAnalyses = TRUE,
+        runDiagnostics = FALSE,
+        packageResults = TRUE,
+        maxCores = maxCores)
+
+resultsZipFile <- file.path(outputFolder, "export", paste0("Results", databaseId, ".zip"))
+dataFolder <- file.path(outputFolder, "shinyData")
+
+prepareForEvidenceExplorer(resultsZipFile = resultsZipFile, dataFolder = dataFolder)
+
+launchEvidenceExplorer(dataFolder = dataFolder, blind = TRUE, launch.browser = FALSE)
